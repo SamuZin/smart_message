378936fc6f8f800b08d1e09e931e6a98477657b3
samuel <samuelomes56@gmail.com>
2025-03-02 02:35:02 +0000

    ICreateMessageUseCase

---
+++ Domain/Contract/UseCases/ICreateMessageUseCase.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entity;
+
+namespace Domain.Contract.UseCases
+{
+    public interface ICreateMessageUseCase
+    {
+        Task<string> CreateQuery(Event event_info);
+        Task<string> CreateMessage(string query);
+        Task<bool> IsValidQuery(string query);
+        Task<bool> IsValidEvent(Event event_info);
+        Task<bool> IsValidMessage(string message, string query);
+    }
+}
---
+++ Domain/Entity/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entity
+{
+    public class Event
+    {
+        DateTime Date { get; set; }
+        string? EventName { get; set; }
+        string? EventDescription { get; set; }
+        string? EventTag { get; set; }
+    }
+}
